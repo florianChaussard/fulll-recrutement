d115a6f032628c6dbea13c4ba2e4e80807322b57
fchaussard <florian.chaussard@inetum.com>
2024-03-18 15:27:17 +0100

    database and entities creation

---
+++ Backend/PHP/Boilerplate/src/Entity/Fleet.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FleetRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FleetRepository::class)]
+class Fleet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $Id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
+    #[ORM\ManyToMany(targetEntity: Vehicle::class, mappedBy: 'lstFleets')]
+    private Collection $lstVehicles;
+
+    public function __construct()
+    {
+        $this->lstVehicles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $Id): static
+    {
+        $this->Id = $Id;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Vehicle>
+     */
+    public function getLstVehicles(): Collection
+    {
+        return $this->lstVehicles;
+    }
+
+    public function addLstVehicle(Vehicle $lstVehicle): static
+    {
+        if (!$this->lstVehicles->contains($lstVehicle)) {
+            $this->lstVehicles->add($lstVehicle);
+            $lstVehicle->addLstFleet($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLstVehicle(Vehicle $lstVehicle): static
+    {
+        if ($this->lstVehicles->removeElement($lstVehicle)) {
+            $lstVehicle->removeLstFleet($this);
+        }
+
+        return $this;
+    }
+}
---
+++ Backend/PHP/Boilerplate/src/Entity/Location.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LocationRepository::class)]
+class Location
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $Id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $longitude = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $latitude = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $Id): static
+    {
+        $this->Id = $Id;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(?float $longitude): static
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(?float $latitude): static
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
